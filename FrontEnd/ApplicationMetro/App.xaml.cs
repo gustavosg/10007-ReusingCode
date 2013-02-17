e8be8f6dae6f7f533264ef7599d87cae76658dff
Gustavo Souza Gonçalves <gustavosouzagoncalves@gmail.com>
2013-02-17 19:51:00 -0300

    update

--- FrontEnd/ApplicationMetro/App.xaml.cs
+++ FrontEnd/ApplicationMetro/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using ApplicationMetro.Common;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,7 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// O modelo de Aplicativo de Grade é documentado em http://go.microsoft.com/fwlink/?LinkId=234226
+// O modelo do Aplicativo em Branco está documentado em http://go.microsoft.com/fwlink/?LinkId=234227
 
 namespace ApplicationMetro
 {
@@ -26,7 +24,7 @@ namespace ApplicationMetro
     sealed partial class App : Application
     {
         /// <resumo>
-        /// Inicializa o objeto de Aplicativo singleton. Esta é a primeira linha do código criado
+        /// Inicializa o objeto de aplicativo singleton.  Esta é a primeira linha de código criado
         /// executado e, como tal, é o equivalente lógico de main() ou WinMain().
         /// </resumo>
         public App()
@@ -41,43 +39,32 @@ namespace ApplicationMetro
         /// resultados da pesquisa e assim por diante.
         /// </resumo>
         /// <param name="args">Detalhes sobre o processo e solicitação de inicialização.</param>
-        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Não repita a inicialização do aplicativo quando a Janela já tiver conteúdo,
             // apenas verifique se a janela está ativa
-            
             if (rootFrame == null)
             {
                 // Crie um Quadro para atuar como o contexto de navegação e navegue para a primeira página
                 rootFrame = new Frame();
-                //Associe o quadro a uma chave SuspensionManager                                
-                SuspensionManager.RegisterFrame(rootFrame, "AppFrame");
 
                 if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    // Restaurar o estado da sessão salvo apenas quando apropriado
-                    try
-                    {
-                        await SuspensionManager.RestoreAsync();
-                    }
-                    catch (SuspensionManagerException)
-                    {
-                        //Erro ao restaurar o estado.
-                        //Suponha que não há nenhum estado e continue
-                    }
+                    //TODO: Carregar estado do aplicativo suspenso anteriormente
                 }
 
                 // Coloque o quadro na Janela atual
                 Window.Current.Content = rootFrame;
             }
+
             if (rootFrame.Content == null)
             {
                 // Quando a pilha de navegação não for restaurada, navegar para a primeira página,
                 // configurando a nova página passando as informações necessárias como um parâmetro
                 // de navegação
-                if (!rootFrame.Navigate(typeof(GroupedItemsPage), "AllGroups"))
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
@@ -93,10 +80,10 @@ namespace ApplicationMetro
         /// </resumo>
         /// <param name="sender">A fonte da solicitação de suspensão.</param>
         /// <param name="e">Detalhes sobre a solicitação de suspensão.</param>
-        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            await SuspensionManager.SaveAsync();
+            //TODO: Salvar o estado do aplicativo e parar qualquer atividade de segundo plano
             deferral.Complete();
         }
     }
---
+++ FrontEnd/ApplicationMetro/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco é documentado em http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ApplicationMetro
+{
+    /// <resumo>
+    /// Uma página vazia que pode ser usada sozinho ou navigated para dentro de um Frame.
+    /// </resumo>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <resumo>
+        /// Chamado quando esta página é exibida num Frame.
+        /// </resumo>
+        /// <param name="e">Dados de evento que descrevem como essa página foi atingida.  O parâmetro
+        /// propriedade normalmente é usada para configurar a página.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
